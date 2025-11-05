4df43fe41cefb0cc2d69be8496949a00a0371932
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2025-11-05 20:39:50 +0400

    Add documentation for increment and decrement MutableMap extensions

--- src/main/kotlin/extensions.kt
+++ src/main/kotlin/extensions.kt
@@ -1,9 +1,27 @@
 @file:Suppress("RedundantVisibilityModifier")
 
+/**
+ * Increments the count for the specified key by 1.
+ *
+ * If the key does not exist in the map, it will be added with a value of 1.
+ * If the key already exists, its value will be increased by 1.
+ *
+ * @param key The key whose count should be incremented
+ */
 public fun <T> MutableMap<T, Int>.increment(key: T) {
     this[key] = (this[key] ?: 0) + 1
 }
 
+/**
+ * Decrements the count for the specified key by 1.
+ *
+ * If the key's count is greater than 1, it will be decreased by 1.
+ * If the key's count is exactly 1, the key will be removed from the map.
+ * If the key does not exist in the map, no changes are made.
+ *
+ * @param key The key whose count should be decremented
+ * @return `true` if the key existed and was decremented or removed, `false` if the key was not found
+ */
 public fun <T> MutableMap<T, Int>.decrement(key: T): Boolean {
     val count = this[key] ?: return false
     if (count > 1) {
